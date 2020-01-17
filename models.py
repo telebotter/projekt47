f15440948ba3d753910c7afbcc9d77d28d50aa11
Lukas <mail@sarbot.de>
2020-01-17 11:32:39 +0100

    db created

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from telebotter import TelebotUser
+from core.models import TelebotUser
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #   telegram related models
@@ -75,15 +75,6 @@ class Stat(models.Model):
                         ', '.join([a.name for a in self.addons.all()]))
 
 
-class CharStat(models.Model):
-    char = models.ForeignKey(Character, on_delete=models.CASCADE)
-    stat = models.ForeignKey(Stat, on_delte=models.CASCADE)
-    value = models.FloatField(default=0)
-
-    def __str__(self):
-        return '{}: {} {}'.format(self.char.name, self.stat.name, self.value)
-
-
 class Action(models.Model):
     """ Any kind of action. To start the action probes vs all stats are rolled.
     if success the formula  is evaluated and the result is used to format the
@@ -91,7 +82,7 @@ class Action(models.Model):
     results are possible but number must match the placeholders in answer.
     """
     name = models.CharField(max_length=200)
-    addons = models.ManyToMany(Addon, related_name='actions')
+    addons = models.ManyToManyField(Addon, related_name='actions')
     stats = models.ManyToManyField(Stat, related_name='actions',
                                 null=True, blank=True)
     formula = models.CharField(max_length=200, null=True, blank=True) # '1*W+4'
@@ -106,11 +97,43 @@ class Character(models.Model):
     """ A players character, belongs to an addon and can have stats with values,
     and actions with values/level through relations.
     """
-    owner = models.ForeignKey(Projekt47User, on_delte=models.CASCADE,
+    owner = models.ForeignKey(Projekt47User, on_delete=models.CASCADE,
                                 null=True, blank=True)
     name = models.CharField(max_length=200)
     game = models.ForeignKey(Game, related_name='characters',
                                 on_delete=models.CASCADE)
+    stats = models.ManyToManyField(Stat, through="CharStat")
 
     def __str__(self):
         return self.name
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#   relations between classes
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+class CharStat(models.Model):
+    """ Relation between character and stat. Stats are unique, as there is only
+    one object `strength` for example but multiple relations to this. Relations
+    that need additional information become its own class, like this one.
+    Documentation to this concept:
+    https://docs.djangoproject.com/en/2.2/topics/db/models/#extra-fields-on-many-to-many-relationships
+    To create such relations, with add/create shortcuts pass a through_default:
+    ```
+    >>> char.stats.add(mystat, through_defaults={'value': 10})
+    >>> char.stats.create(name="Stärke", abbr="St" through_defaults={'value': 10})
+    >>> char.stats.set([st, in, wi, ge], through_defaults={'value': 10)})
+    ```
+    TODO: does this work also in reverse?
+    ```
+    >>> stat.chars.add(mychar, through_defaults={'value': 10})
+    >>> stat.chars.set([char1, char2], through_defaults={'value': 10)})
+    ```
+    """
+    char = models.ForeignKey(Character, on_delete=models.CASCADE)
+    stat = models.ForeignKey(Stat, on_delete=models.CASCADE)
+    value = models.FloatField(default=0)
+
+    def __str__(self):
+        return '{}: {} {}'.format(self.char.name, self.stat.name, self.value)
