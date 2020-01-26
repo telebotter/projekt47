9794d50e173a3807f0f8e1229df4475e3474e9f4
sarbot <mail@sarbot.de>
2020-01-27 00:55:29 +0100

    shared handlers

--- telegrambot.py
+++ telegrambot.py
@@ -22,6 +22,11 @@ from django.conf import settings
 logger = logging.getLogger(__name__)
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#   run the bot
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
@@ -32,20 +37,48 @@ def start(bot, update):
     update.message.reply_text(f'hallo {p_user.telebot_user.first_name}')
 
 
-def main():
-    logger.info("Loading handlers for wuerfeln")
-    dp = DjangoTelegramBot.getDispatcher('projekt47bot')
+def devtest(bot, update):
+    update.message.reply_text('Ich bin der pollende test bot')
+
+
+def webtest(bot, update):
+    update.message.reply_text('Ich bin der offizielle web bot')
+
+
+def sharetest(bot, update):
+    update.message.reply_text('Ich bin eine normale antwort')
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#   run the bot
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+def add_shared_handlers(dp):
+    """ This function adds handlers that are used in dev (polling) and
+    production (webhook). DTB dispatcher is a subclass of the PTB dp, they share
+    most of their methods and can be used in a similar way.
+    """
+    # handlers for both methods
     dp.add_handler(CommandHandler('start', start))
     dp.add_error_handler(error)
+
+
+def main():
+    """ function called by django-telegram-bot automatically on webhook
+    """
+    dp = DjangoTelegramBot.getDispatcher('projekt47bot')
+    add_shared_handlers(dp)
+    # add webhook specific handlers below
+    dp.add_handler(CommandHandler('webtest', webtest))
 
 
 def devmode():
     """ function called by manage.py in an local environment (dev mode)
     """
-    # INIT TELEGRAM BOT
-    updater = Updater(token=settings.PROJEKT47_TOKEN)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_error_handler(error)
-    updater.start_polling()
-    logger.info('devmode finish')
+    up = Updater(token=settings.PROJEKT47_TOKEN)
+    dp = up.dispatcher
+    add_shared_handlers(dp)
+    # add polling specific handlers (or under development) below
+    dp.add_handler(CommandHandler('devtest', devtest))
+    # start the update loop
+    up.start_polling()
