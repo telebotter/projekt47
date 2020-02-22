1b45a2feae7f25d4d7305df0f75f1f43347ac8e1
Lukas <mail@sarbot.de>
2020-02-22 16:31:30 +0100

    filter choices admin

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.urls import resolve
 from projekt47.models import *
 
 # Register your models here.
@@ -10,6 +10,8 @@ admin.site.register(Adventure)
 # admin.site.register(CharStat)
 admin.site.register(DefaultName)
 
+import logging
+logger = logging.getLogger(__name__)
 
 
 @admin.register(Action)
@@ -41,16 +43,27 @@ class StatInline(admin.TabularInline):
 class ActionInline(admin.TabularInline):
     model = Action
     extra = 1
-    form = ActionAdmin.form
 
-    # def get_form(self, request, obj=None, **kwargs):
-    #     self.instance = obj
-    #     return super(ActionInline, self).get_form(request, obj=obj, **kwargs)
-    #
-    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
-    #     if db_field.name in ["stat_1", "stat_2", "stat_3"] and hasattr(self, 'instance'):
-    #         kwargs["queryset"] = Stat.objects.filter(addon=self.instance.addon)
-    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    def get_parent_object_from_request(self, request):
+        """
+        Returns the parent object from the request or None.
+
+        Note that this only works for Inlines, because the `parent_model`
+        is not available in the regular admin.ModelAdmin as an attribute.
+        """
+        resolved = resolve(request.path_info)
+        if resolved.kwargs:
+            return self.parent_model.objects.get(pk=resolved.kwargs['object_id'])
+        return None
+
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        # is called but how to get addon instance?  prob use get_parent hack from here
+        # https://stackoverflow.com/questions/14950193/how-to-get-the-current-model-instance-from-inlineadmin-in-django
+        if db_field.name in ["stat_1", "stat_2", "stat_3"]:# and self.instance:
+            addon = self.get_parent_object_from_request(request)
+            kwargs["queryset"] = Stat.objects.filter(addon=addon)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 class MetaCardInline(admin.TabularInline):
@@ -66,14 +79,9 @@ class ResInline(admin.TabularInline):
 class AddonAdmin(admin.ModelAdmin):
     inlines = [StatInline, ResInline, ActionInline, MetaCardInline]
 
-    # def get_form(self, request, obj=None, **kwargs):
-    #     self.instance = obj
-    #     return super(AddonAdmin, self).get_form(request, obj=obj, **kwargs)
-    #
-    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
-    #     if db_field.name in ["stat_1", "stat_2", "stat_3"] and self.instance:  # and hasattr(self, 'instance'):
-    #         kwargs["queryset"] = Stat.objects.filter(addon=self.instance.addon)
-    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    def get_form(self, request, obj=None, **kwargs):
+        self.instance = obj
+        return super(AddonAdmin, self).get_form(request, obj=obj, **kwargs)
 
 
 @admin.register(Character)
